e0137038cb166eff40c32e0be71d335e8f3b68a9
Kit <jkgraphicdesign18@gmail.com>
2021-01-21 10:47:39 -0800

    01/21

---
+++ Assets/Script/HeroTemplate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName ="New Character", menuName ="Character")]
+public class HeroTemplate : ScriptableObject
+{
+    // Start is called before the first frame update
+    public new string name;
+    public Sprite HeroSprite;
+}
--- Assets/Script/WaitingRoomScript.cs
+++ Assets/Script/WaitingRoomScript.cs
@@ -7,24 +7,17 @@ public class WaitingRoomScript : MonoBehaviour
 {
     [SerializeField]
     private Button sampleButt;
+   
     [SerializeField]
-    private GridLayoutGroup gridlay;
+    private List<HeroTemplate> heroList; 
 
     Sprite charimg1, charimg2, charimg3;
     void Start()
     {
-        //gridlay.constraintCount = 3;
-        Sprite[] tk = Resources.LoadAll<Sprite>("CharacterImgs");
-        Debug.Log(tk.Length);
-        foreach( Sprite ti in tk)
-        {
-            Debug.Log(ti.name);
-        }
-        for ( int i = 0; i < tk.Length; i ++)
-        {
-            Button bui = Instantiate(sampleButt, gameObject.transform) as Button;
-            bui.image.sprite = tk[i];
-        }
+        heroList = new List<HeroTemplate>();
+        
+        
+        
         
 
     }
